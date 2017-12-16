de892976b49df8fcc9f75408c1bf20ad5586a42d
Ksena <oksishukh@yahoo.com>
2017-12-16 19:24:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = new Object({artistName: 'songTitle'});
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle 
+  playlist[artistName] = songTitle
+  return playlist
 }
 
 function removeFromPlaylist (playlist , artistName){
